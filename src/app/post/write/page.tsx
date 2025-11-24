90e9f50270be3e14c6bd6b4e7417b7ce0e25f03c
SangWon7242 <gotkdqja@gmail.com>
2025-11-24 16:11:04 +0900

    게시물 작성

--- src/app/post/write/page.tsx
+++ src/app/post/write/page.tsx
@@ -5,29 +5,38 @@ import { Post } from "@/app/types/post";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
+import { createClient } from "@supabase/supabase-js";
+import { useRouter } from "next/navigation";
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
+);
 
 export default function PostWritePage() {
+  const router = useRouter();
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
 
-  const [posts, setPosts] = useState<Post[]>([]);
-
-  const handleWrite = () => {
+  const handleWrite = async () => {
     if (!title.trim() || !content.trim()) return;
 
-    const lastPostId = posts[posts.length - 1]?.id || 0;
+    const { error } = await supabase.from("post").insert({
+      title,
+      content,
+    });
 
-    setPosts((post) => [
-      ...post,
-      {
-        id: lastPostId + 1,
-        title,
-        content,
-      },
-    ]);
+    if (error) {
+      console.log(error);
+      alert("글 저장 중 오류 발생!!");
+      return;
+    }
 
     setTitle("");
     setContent("");
+
+    alert("글이 작성되었습니다.");
+    router.push("/");
   };
 
   return (
@@ -46,7 +55,6 @@ export default function PostWritePage() {
         />
         <Button onClick={handleWrite}>작성</Button>
       </section>
-      <div>{JSON.stringify(posts)}</div>
     </>
   );
 }
