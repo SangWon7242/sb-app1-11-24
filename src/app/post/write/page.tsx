5a61892264887097f34d0559d44cc84ed1694e0c
SangWon7242 <gotkdqja@gmail.com>
2025-11-24 15:25:47 +0900

    글 작성 폼 구현

--- src/app/post/write/page.tsx
+++ src/app/post/write/page.tsx
@@ -1,3 +1,52 @@
+"use client";
+
+import { useState } from "react";
+import { Post } from "@/app/types/post";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+
 export default function PostWritePage() {
-  return <div>글 쓰기</div>;
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  const handleWrite = () => {
+    if (!title.trim() || !content.trim()) return;
+
+    const lastPostId = posts[posts.length - 1]?.id || 0;
+
+    setPosts((post) => [
+      ...post,
+      {
+        id: lastPostId + 1,
+        title,
+        content,
+      },
+    ]);
+
+    setTitle("");
+    setContent("");
+  };
+
+  return (
+    <>
+      <section className="post-write-section">
+        <h1 className="text-2xl font-bold">게시글 작성</h1>
+        <Input
+          placeholder="제목을 입력해주세요."
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <Textarea
+          placeholder="내용을 입력해주세요."
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        />
+        <Button onClick={handleWrite}>작성</Button>
+      </section>
+      <div>{JSON.stringify(posts)}</div>
+    </>
+  );
 }
