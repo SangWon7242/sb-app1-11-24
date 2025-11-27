c872052c79bbcb23e78a245fd3e7a931ca51ef23
SangWon7242 <gotkdqja@gmail.com>
2025-11-27 15:33:18 +0900

    마크다운 에디터 도입

--- src/app/post/write/page.tsx
+++ src/app/post/write/page.tsx
@@ -3,9 +3,9 @@
 import { useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { Textarea } from "@/components/ui/textarea";
 import { createClient } from "@supabase/supabase-js";
 import { useRouter } from "next/navigation";
+import MDEditor from "@uiw/react-md-editor";
 
 // 브라우저용으로 기본 Supabase 클라이언트를 직접 생성
 const supabase = createClient(
@@ -19,7 +19,15 @@ export default function PostWritePage() {
   const [content, setContent] = useState("");
 
   const handleWrite = async () => {
-    if (!title.trim() || !content.trim()) return;
+    if (!title.trim()) {
+      alert("제목을 입력해주세요.");
+      return;
+    }
+
+    if (!content.trim()) {
+      alert("내용을 입력해주세요.");
+      return;
+    }
 
     const { error } = await supabase.from("post").insert({
       title,
@@ -41,19 +49,27 @@ export default function PostWritePage() {
 
   return (
     <>
-      <section className="post-write-section">
-        <h1 className="text-2xl font-bold">게시글 작성</h1>
-        <Input
-          placeholder="제목을 입력해주세요."
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-        <Textarea
-          placeholder="내용을 입력해주세요."
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-        />
-        <Button onClick={handleWrite}>작성</Button>
+      <section className="post-write-section flex flex-col w-full ">
+        <div className="inner flex flex-col gap-2 p-3 h-full">
+          <h1 className="text-2xl font-bold text-center">게시글 작성</h1>
+          <Input
+            placeholder="제목을 입력해주세요."
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <div className="min-h-[calc(100vh-320px)]">
+            <MDEditor
+              value={content}
+              height="100%"
+              onChange={(val) => setContent(val || "")}
+              preview="live" // 분할 화면으로 변경
+              hideToolbar={false} // 툴바 표시/숨김
+              enableScroll={true} // 스크롤 동기화
+              visibleDragbar={false} // 크기 조절 바 표시
+            />
+          </div>
+          <Button onClick={handleWrite}>작성</Button>
+        </div>
       </section>
     </>
   );
