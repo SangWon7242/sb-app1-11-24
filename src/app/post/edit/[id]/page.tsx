03bc1095863f6fef84870bfc44821cee0a267321
SangWon7242 <gotkdqja@gmail.com>
2025-12-09 14:52:45 +0900

    step2: 게시물 수정 처리

--- src/app/post/edit/[id]/page.tsx
+++ src/app/post/edit/[id]/page.tsx
@@ -20,12 +20,15 @@ export default function PostEditPage({
 }) {
   const router = useRouter();
   const [loading, setLoading] = useState(true);
+  const [id, setId] = useState("");
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
 
   useEffect(() => {
     const loadPost = async () => {
       const { id: postId } = await params;
+
+      setId(postId);
 
       // SELECT * FROM post WHERE id = ?;
       const { data: post, error } = await supabase
@@ -51,6 +54,46 @@ export default function PostEditPage({
     loadPost();
   }, [params, router]);
 
+  // 게시물 수정 처리
+  const handleEdit = async () => {
+    // 제목, 내용에 대한 유효성 검사
+    if (!title.trim()) return alert("제목을 입력해주세요.");
+    if (!content.trim()) return alert("내용을 입력해주세요.");
+
+    // UPDATE post SET title = ?, content = ? WHERE id = ?;
+    const { error } = await supabase
+      .from("post")
+      .update({ title, content })
+      .eq("id", id)
+      .select()
+      .single();
+
+    if (error) {
+      console.error("게시물 수정 실패 :", error);
+      return;
+    }
+
+    alert(`${id}번 게시물이 수정되었습니다.`);
+    router.push(`/post/${id}`);
+  };
+
+  useEffect(() => {
+    const saveKeyDown = (e: KeyboardEvent) => {
+      // Ctrl + s, cmd + s
+      if ((e.ctrlKey || e.metaKey) && e.key === "s") {
+        e.preventDefault(); // 브라우저에서 ctrl + s 눌렀을 때 기본 이벤트를 막음
+        handleEdit();
+      }
+    };
+
+    window.addEventListener("keydown", saveKeyDown);
+
+    // 클린업 함수
+    return () => {
+      window.removeEventListener("keydown", saveKeyDown);
+    };
+  }, [title, content]);
+
   return (
     <>
       <section className="post-write-section flex flex-col w-full ">
@@ -72,7 +115,7 @@ export default function PostEditPage({
               visibleDragbar={false} // 크기 조절 바 표시
             />
           </div>
-          <Button>수정</Button>
+          <Button onClick={handleEdit}>수정</Button>
           <Button>
             <Link href="/">취소</Link>
           </Button>
