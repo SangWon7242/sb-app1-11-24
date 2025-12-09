c7b65bbd61b5ba670e720fb740cd38856acafb7d
SangWon7242 <gotkdqja@gmail.com>
2025-12-09 15:57:52 +0900

    PostService 도입, getPostById 함수를 이용한 게시물 조회

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,32 +1,20 @@
-import { createClient } from "@/app/utils/supabase/server";
-import { notFound } from "next/navigation";
 import { formatDate } from "@/app/utils/dateForatter";
 import MarkdownViewer from "@/components/MarkdownViewer";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { getPostById } from "@/app/services/PostService";
+import { redirect } from "next/navigation";
 
 export default async function PostDetailPage({
   params,
 }: {
   params: Promise<{ id: string }>;
 }) {
-  const supabase = await createClient();
   const { id } = await params;
-
-  // SELECT * FROM post WHERE id = id;
-  const { data: post, error } = await supabase
-    .from("post")
-    .select("*")
-    .eq("id", id) // WHERE id = id;
-    .single(); // single() : 배열이 아닌 단일 객체를 반환
-
-  if (error) {
-    console.error("게시물 조회 에러 :", error);
-    notFound();
-  }
+  const post = await getPostById(Number(id));
 
   if (!post) {
-    notFound();
+    redirect("/post/list");
   }
 
   return (
---
+++ src/app/services/PostService.ts
@@ -0,0 +1,19 @@
+import { Post } from "@/app/types/post";
+import { createClient } from "@/app/utils/supabase/server";
+
+// 게시물 조회
+export async function getPostById(id: number): Promise<Post | null> {
+  const supabase = await createClient();
+  const { data, error } = await supabase
+    .from("post")
+    .select("*")
+    .eq("id", id)
+    .single();
+
+  if (error) {
+    console.error("게시물 조회 실패 : ", error);
+    return null;
+  }
+
+  return data;
+}
